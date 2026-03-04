93f8055dcc2f77327220673be1c61da0e94fc94f
James <qianfan.cheng.cn@gmail.com>
2026-03-04 05:36:21 +0000

    init

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+
+	_ "github.com/GoAdminGroup/go-admin/adapter/gin"                 // web framework adapter
+	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/postgres" // sql driver
+	_ "github.com/GoAdminGroup/themes/adminlte"                      // ui theme
+
+	"github.com/GoAdminGroup/go-admin/engine"
+	"github.com/GoAdminGroup/go-admin/template"
+	"github.com/GoAdminGroup/go-admin/template/chartjs"
+	"github.com/gin-gonic/gin"
+
+	"github.com/james-wukong/online-school-mgmt/pages"
+	"github.com/james-wukong/online-school-mgmt/tables"
+)
+
+func main() {
+	startServer()
+}
+
+func startServer() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DefaultWriter = ioutil.Discard
+
+	r := gin.Default()
+
+	template.AddComp(chartjs.NewChart())
+
+	eng := engine.Default()
+
+	if err := eng.AddConfigFromYAML("./config.yml").
+		AddGenerators(tables.Generators).
+		Use(r); err != nil {
+		panic(err)
+	}
+
+	r.Static("/uploads", "./uploads")
+
+	eng.HTML("GET", "/admin", pages.GetDashBoard)
+	eng.HTMLFile("GET", "/admin/hello", "./html/hello.tmpl", map[string]interface{}{
+		"msg": "Hello world",
+	})
+
+	_ = r.Run(":8091")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Print("closing database connection")
+	eng.PostgresqlConnection().Close()
+}
